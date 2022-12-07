8cecc36ed87f0f1a840386d4f4e97fb266ef5cc9
Sarah Zimmer <Sarah Zimmer@DESKTOP-Q3NRSDA.lan>
2022-12-06 20:05:01 -0500

    Created functions.py, each function now called into main.py

---
+++ Meeks_FinalProject/MainPackage/functions.py
@@ -0,0 +1,26 @@
+from PIL import Image
+import json
+
+# Iteration to add each word to the dictionary along with the place #
+def dictionary(txtFile):
+    deciphererDict = {} # Dictionary to store key value pair: (place #, associated word)
+    with open(txtFile) as textFileKey: # Make Python open and look at text file containing word list
+        for index, line in enumerate(textFileKey): # Iterate and assign number to each word
+            formattedString = line.replace('\n','') # Fix word spacing before storing in Dictionary
+            deciphererDict.update({index:formattedString}) # Add place # and word to dictionary as key value pair
+    return deciphererDict
+
+# Get out dictionary data from EncryptedGroupHints.json
+def encryptData(jsFile, groupName):
+    with open(jsFile) as json_file:
+        encryptedData = json.load(json_file)
+        groupData = encryptedData[groupName] # Get our portion of the dictionary out
+    return groupData
+
+# Decipher the location by iterating over the dictionary and putting each word in a list
+def decryptMessage(groupData, dictionary):
+    answerList = [] # list to store results
+    for i in groupData: 
+        word_code = int(i) 
+        answerList.append(dictionary[word_code]) #append list to store each answer
+    return answerList
--- Meeks_FinalProject/MainPackage/main.py
+++ Meeks_FinalProject/MainPackage/main.py
@@ -1,48 +1,24 @@
 '''
 Name: Sarah Zimmer, Sara Jundi, John O'Connell, Brett Perez
-email: perezbd@mail.uc.edu
+email: perezbd@mail.uc.edu, zimmese@mail.uc.edu
 Assignment: Final Project
 Course: IS 4010
 Semester/Year: Fall 2022
 Brief Description: In this project, we use everything we learned during the semester to decipher a hidden message and display an image that proves we deciphered the message.
-Citations: https://www.youtube.com/watch?v=uPT-LkYSP_o
+Citations: 
+https://www.youtube.com/watch?v=uPT-LkYSP_o
+https://www.geeksforgeeks.org/convert-json-to-dictionary-in-python/
+https://stackoverflow.com/questions/41276755/convert-txt-file-into-a-list-of-integers-in-python
 Anything else that's relevant: 
 '''
 
-from PIL import Image
-import json
+from Functions import *
 
-'''
-#read the image
-im = Image.open("John O'Connell High Res.jpg")
+englishDict = dictionary('english.txt')
+#print(englishDict)
 
-#show image
-im.show()
-'''
+meeksData = encryptData('EncryptedGroupHints.json', 'Meeks')
+#print(meeksData)
 
-# Dictionary to store key value pair: (place #, associated word)
-deciphererDict = {}
-
-# Iteration to add each word to the dictionary along with the place #
-with open('english.txt') as textFileKey: # Make Python open and look at text file containing word list
-    for index, line in enumerate(textFileKey): # Iterate and assign number to each word
-        formattedString = line.replace('\n','') # Fix word spacing before storing in Dictionary
-        deciphererDict.update({index:formattedString}) # Add place # and word to dictionary as key value pair
-print(deciphererDict)
-
-# Get out dictionary data from EncryptedGroupHints.json 
-with open('EncryptedGroupHints.json') as json_file:
-    encryptData = json.load(json_file)
-#print(encryptData)
-# Get our portion of the dictionary out
-meeksData = encryptData['Meeks']
-print(meeksData)
-
-# Decipher the location by iterating over the dictionary and putting each word in a list
-encryptedMessage= meeksData
-answerList = []
-
-for i in encryptedMessage:
-    word_code = int(i)
-    answerList.append(deciphererDict[word_code])
-print(answerList)
+meeksMessage = decryptMessage(meeksData, englishDict)
+print(meeksMessage)
